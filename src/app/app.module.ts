83d3f4315ff652b32ab8a671b28328bcd974ad29
Kamlesh <banosekar.kamlesh@gmail.com>
2021-06-19 16:03:52 +0530

    project-structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/shared/header/header.component';
+import { FooterComponent } from './pages/shared/footer/footer.component';
+import { NavigationComponent } from './pages/shared/navigation/navigation.component';
+import { CartComponent } from './pages/shared/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule
